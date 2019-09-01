6f9bdd042e58217cb8a935e3020976e0b0e30b39
OBA Daisuke <44015510+ba-san@users.noreply.github.com>
2019-09-01 12:07:38 +0900

    Update annotation.py

--- annotation.py
+++ annotation.py
@@ -8,12 +8,10 @@ import copy
 import linecache, shutil
 
 folder = "images" #input images in this directory
-
 PWD = os.getcwd() + '/'
-
 files=glob.glob(PWD + folder + "/*")
-
 path = PWD + folder + "_cropped"
+csvpath = os.path.join(path, folder) + '.csv'
 
 s = 0
 k = 0
@@ -33,18 +31,12 @@ print("  B   -- go back ONLY ONE act")
 print(" Esc  -- clear image and start it again")
 print("Enter -- go to next\n")
 
-csvpath = os.path.join(path, folder) + '.csv'
-
 for fname in files:
-
-	img = cv2.imread(fname)
-	
+	img = cv2.imread(fname)	
 	x=1
 	y=1
-
 	
 	if s == 0: #continue
-
 		right = os.path.basename(fname[-10:-4])
 		lastrow = sum(1 for i in open(csvpath)) # http://hk29.hatenablog.jp/entry/2018/05/01/152759
 		#lastrow_sen = linecache.getline(os.path.join(path, folder) + '.csv', lastrow)
@@ -63,14 +55,11 @@ for fname in files:
 		if k == 0: #start from previous
 			lastrow = sum(1 for i in open(csvpath)) # http://hk29.hatenablog.jp/entry/2018/05/01/152759
 			#lastrow_sen = linecache.getline(os.path.join(path, folder) + '.csv', lastrow)
-			last = glob.glob(path + "/LAST_*")
-			last = ','.join(last)
-			
+			last = ','.join(glob.glob(path + "/LAST_*"))
 			lastimg = cv2.imread(last)
 			y = int(last[-5])
 			x = int(last[-7])
 			tpcnt = int(lastrow) - 1
-
 			
 		elif k == 1: #continue to next frame
 			x = 1
@@ -90,10 +79,8 @@ for fname in files:
 			window_name = os.path.join(path, os.path.basename(fname) + "_" + str(x) + "_" + str(y) + ".jpg")
 			if s==0 and cnt==0:  #only for first image when you start from continue
 				cnt=1
-				last = glob.glob(path + "/LAST_*")
-				last = ','.join(last)
-				resized_cropped = cv2.imread(last)
-				resized_cropped = cv2.resize(resized_cropped, (900, 900))
+				last = ','.join(glob.glob(path + "/LAST_*"))
+				resized_cropped = cv2.resize(cv2.imread(last), (900, 900))
 				resized_previous = cv2.imread(path + "/PREVIOUS.jpg")
 				os.remove(last)
 			
@@ -106,7 +93,6 @@ for fname in files:
 			print(window_name)
 			cv2.imshow(window_name, resized_cropped)
 			mouseData = mouseevent.mouseParam(window_name)
-			
 			
 			while True:
 				k = cv2.waitKey(0) # waiting input
